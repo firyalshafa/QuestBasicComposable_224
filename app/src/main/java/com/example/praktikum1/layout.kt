ca8f7130b00571e1f42e4dd8e0b3d023ce8b5e9a
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-16 14:45:13 +0700

    membuat Fungsi TataletakColumn

--- app/src/main/java/com/example/praktikum1/layout.kt
+++ app/src/main/java/com/example/praktikum1/layout.kt
@@ -23,3 +23,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun TataletakColumn(modifier: Modifier){}
