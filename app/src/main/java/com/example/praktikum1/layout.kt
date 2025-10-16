23223c26dc15fdd1647b1a851a6ec2d255cbbeab
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-16 15:07:48 +0700

    add Row layout containing multiple Columns

--- app/src/main/java/com/example/praktikum1/layout.kt
+++ app/src/main/java/com/example/praktikum1/layout.kt
@@ -71,3 +71,21 @@ fun TaataaletakColumnRow(modifier: Modifier){
     }
 
     }
+
+@Composable
+fun TataletakRowColumn(modifier: Modifier){
+    Row (modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
+        // Klolom 1
+        Column (){
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
+        // Kolom2
+        Column (){
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "komponen3kolom2")
+        }
+    }
+}
