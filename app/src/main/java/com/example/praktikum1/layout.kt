1c38b7d2268c48dc93928ef4a67ee963e3ac2213
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-16 15:06:22 +0700

    add second row layout with text components

--- app/src/main/java/com/example/praktikum1/layout.kt
+++ app/src/main/java/com/example/praktikum1/layout.kt
@@ -61,6 +61,13 @@ fun TaataaletakColumnRow(modifier: Modifier){
             Text(text = "Komponen2Baris1")
             Text(text = "Komponen3Baris1")
         }
-
+        //Baris 2
+        Row (modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
+    }
 
     }
